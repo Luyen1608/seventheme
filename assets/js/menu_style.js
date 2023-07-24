8923d165b3312cb6085a566f97bf79c6f652ac2e
luyendv-dtsvn <luyendv@dtsvn.net>
2023-07-24 14:35:09 +0700

    update product list

--- assets/js/menu_style.js
+++ assets/js/menu_style.js
@@ -1,2 +1,18 @@
 $(".filter-main-btn").append("<div class='cart-btn' data-content='10'><img src='assets/images/cart-2.svg' alt=''></div>");
 $("body").append("<ul class='nk-sticky-toolbar'><li class='demo-purchase' data-content='2'><a href='/Cart'><img src='assets/images/cart-red.svg' alt=''></a></li></ul>");
+
+
+
+// <div class="btns-wrap only-mobile">
+//             <div class="mobile-cate-btn">
+//                 <div class="container-al">
+//                     <i class="fa-solid fa-bars"></i>
+//                 </div>
+//             </div>
+
+//             <div class="shopping-cart" data-content="3">
+//                 <img src="./assets/images/Shipping.svg" alt="">
+//             </div>
+//             <a href="#" class="sign-in-btn btn-mobile"> Đăng nhập </a>
+//             <a href="#" class="sign-up-btn btn-mobile"> Đăng ký </a>
+//         </div>
