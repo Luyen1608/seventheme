d8f84fba8581056833c890e44221fea32c2892ad
Luyen1608 <96036620+Luyen1608@users.noreply.github.com>
2024-11-28 18:01:39 +0700

    update đến cart

---
+++ baclieu/assets/js/menu_style.js
@@ -0,0 +1,2 @@
+$(".filter-main-btn").append("<div class='cart-btn' data-content='10'><img src='../../assets/img/cart-2.svg' alt=''></div>");
+$("body").append("<ul class='nk-sticky-toolbar'><li class='demo-purchase'><a href='/Cart'><img src='../../assets/img/cart-red.svg' alt=''></a></li></ul>");
